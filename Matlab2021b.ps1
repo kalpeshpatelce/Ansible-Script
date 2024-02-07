4b3a45b43a2de660e5f82f58c515668154e8f1e4
kalpeshpatelce <kalpeshpatel.ce@hotmail.com>
2024-02-07 11:26:07 +0530

    Add files via upload

---
+++ Matlab2021b.ps1
@@ -0,0 +1,5 @@
+﻿cmdkey /add:172.16.2.137 /user:Administrator /pass:ceit
+net use Z: /delete
+net use Z: \\172.16.2.137\matlab /persistent:no
+Set-Location -Path "Z:"
+Z:\setup.exe -inputFile Z:\installer_input.txt
